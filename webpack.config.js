9419c77d6b987f3c6658cb9122050e07cba2abfc
Krunal Patel <krunal@codetails.com>
2025-06-30 13:36:30 +0100

    added separate cmd to install certificate

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,42 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const devCerts = require('office-addin-dev-certs');
 
-module.exports = async () => {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-
-  return {
-    entry: './src/index.tsx',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      clean: true,
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|tsx)$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html'),
-      }),
-    ],
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'public'),
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-      port: 3001,
-      open: true,
-      hot: true,
-      https: httpsOptions,
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    mode: 'development',
-  };
+    port: 3001,
+    open: true,
+    hot: true,
+    https: true,
+  },
+  mode: 'development',
 }; 
